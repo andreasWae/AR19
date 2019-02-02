618fb806b0705bd9ca034b2d551801790d3e0910
Unknown <andreas.waehle@alignracing.no>
2019-02-02 14:28:20 +0100

    LED test update

--- ACM_code/ACM_code/src/ACM/ACM.ino
+++ ACM_code/ACM_code/src/ACM/ACM.ino
@@ -7,9 +7,13 @@ StatusLED onboardLED = StatusLED ();
 void setup()
 {
   onboardLED.setup();
+  for (uint8_t i = 5; i--;)
+  {
+    onboardLED.LEDColorRotation(100);
+  }
 }
 
 void loop()
 {
-  
+  onboardLED.LEDColorRotation(12000);
 }
